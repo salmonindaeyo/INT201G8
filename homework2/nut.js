dfc7434146f76e50c9da08baaf1e6a4c9900d48a
salmon <iamfhaa@gmail.com>
2021-09-05 00:17:13 +0700

    ทำไม่ได้

---
+++ homework2/nut.js
@@ -0,0 +1,67 @@
+
+ //คำถามและคำตอบ
+
+let Question = {
+
+    1 : '1+0=?',
+
+    choice1 : {
+
+        choice1 : 1, choice2 : 2, choice3 : 3, choice4 : 4, choice5 : 5
+
+        , answer : 1
+
+    },
+
+    2 : '1+1=?',
+
+    choice2 : {
+
+        choice1 : 1, choice2 : 2, choice3 : 3, choice4 : 4, choice5 : 5
+
+        , answer : 2
+
+    },
+
+    3 : '1+2=?',
+
+    choice3 : {
+
+        choice1 : 1, choice2 : 2, choice3 : 3, choice4 : 4, choice5 : 5
+
+        , answer : 3
+
+    },
+
+    4 : '1+3=?',
+
+    choice4 : {
+
+        choice1 : 1, choice2 : 2, choice3 : 3, choice4 : 4, choice5 : 5
+
+        , answer : 4
+
+    },
+
+
+   5 : {Question : '1+4=?', answer : 5 , choice : {1 : 1, 2 : 2, 3 : 3, 4 : 4, 5 : 5 }}
+
+   
+};
+
+
+ function play (Player,NumberOfQuestion,NumberOfAnswer) {
+
+            let point = 0;
+
+            if(Question.NumberOfQuestion.answer == Question.NumberOfQuestion.choice.NumberOfAnswer ) {
+
+               return point +=1;
+                    
+            }
+
+            
+ }
+
+
+
