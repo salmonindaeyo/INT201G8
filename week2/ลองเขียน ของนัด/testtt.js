3f370b28f9f2ecef55214a281781394ade7d5bd3
salmon <iamfhaa@gmail.com>
2021-09-05 00:15:57 +0700

    ss

---
+++ week2/ลองเขียน ของนัด/testtt.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+function rase(years){
+    let rase = {
+        monkey: "monkey" ,
+        rooster :"rooster"
+        , dog : "dog"
+        , pig : "pig"
+        , rat : "rat"
+        , ox : "ox"
+        , tiger : "tiger"
+        , rab : "rab"
+        , dra : "dra"
+        , snake : "snake"
+        , horse : "horse"
+        , sheep : "sheep"
+    
+    }
+    let num = years % 12
+    switch(num){
+        case 0 : console.log(rase.monkey)
+        break;
+        case 1 : console.log(rase.rooster)
+        break;
+        case 2 : console.log(rase.dog)
+        break;
+        case 3 : console.log(rase.pig)
+        break;
+        case 4 : console.log(rase.rat)
+        break;
+        case 5 : console.log(rase.ox)
+        break;
+        case 6 : console.log(rase.tiger)
+        break;
+        case 7 : console.log(rase.rab)
+        break;
+        case 8 : console.log(rase.dra)
+        break;
+        case 9 : console.log(rase.snake)
+        break;
+        case 10 : console.log(rase.horse)
+        break;
+        case 11 : console.log(rase.sheep)
+        break;
+    }
+}
+
+rase(1900)
+rase(2001)
+rase(2024)
