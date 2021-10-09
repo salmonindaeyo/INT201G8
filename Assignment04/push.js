ad218d5ed6b641221412c9e1774d66f61d9066f6
salmon <iamfhaa@gmail.com>
2021-10-09 13:23:51 +0700

    pushArray

---
+++ Assignment04/push.js
@@ -0,0 +1,43 @@
+let vegetables = ['parsnip', 'potato']
+let moreVegs = ['celery', 'beetroot']
+
+// Merge the second array into the first one
+vegetables.push(...moreVegs);
+
+
+
+
+
+
+
+let obj = {
+    length: 0,
+
+    addElem: function addElem(elem) {
+        // obj.length is automatically incremented
+        // every time an element is added.
+        [].push.call(this, elem)
+    }
+}
+
+// Let's add some empty objects just to illustrate.
+obj.addElem({})
+obj.addElem({})
+console.log(obj.length)
+
+
+
+
+
+let fruits = ['Apple','bannana' , 'carrot']
+
+fruits.push('watermelon');
+
+
+
+
+
+
+
+
+
