43a6ecd87895c5f2a6374607ec581af182c42ef8
Catintheboxx <wased1009@gmail.com>
2021-10-09 11:30:42 +0700

    [add] pop()

---
+++ Assignment04/pop.js
@@ -0,0 +1,23 @@
+//Syntax
+//pop()
+
+//ex1
+let arr = ['A', 'B', 'C', 'D', 'E', 'F']
+let lastArr = arr.pop();
+console.log(lastArr); // F
+console.log(arr); // ['A', 'B', 'C', 'D', 'E']
+
+//ex2
+let arr2 = ['A', 'B', 'C', 'D', 'E', 'F']
+console.log(`Before: ${arr2.length}`) // 6
+arr2.pop();
+console.log(`after ${arr2.length}`); // 5
+
+//ex3
+let arr3 = ['A', 'B', 'C', 'D', 'E', 'F'];
+let newArr = [];
+for (let i = 0; i < arr3.length; i++) {
+    newArr.push(arr3.pop());
+    i--;
+}
+console.log(newArr)
