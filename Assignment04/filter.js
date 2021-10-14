600082d708b30d61ee57efbeaa1e9802b5d81f9b
vichuta <vichuta.nice@mail.kmutt.ac.th>
2021-10-14 13:46:52 +0700

    edit code

--- Assignment04/filter.js
+++ Assignment04/filter.js
@@ -9,12 +9,16 @@
 
 */
 
+
 //ตัวอย่าง Callblack function 
 let ages = [10,12,18,22,35,40,45,50,60];
 function checkAge(age){ //age คือ element แต่ละตัวที่อยู่ใน ages
     return age >=18;    //return age ที่มีค่าอย่างน้อย 18
 }
 console.log(ages.filter(checkAge));  //[18, 22, 35, 40, 45, 50, 60]
+
+
+
 
 //ตัวอย่าง array.filter(function(value, index, arr)) 
 let num = [0,1,2,3,4,5,6,7,8,9,10,11,12,13];
@@ -23,31 +27,40 @@ const evenNum = num.filter(function (element,index,array){
 });
 console.log(evenNum);
 
+
+
+
 //ตัวอย่าง array.filter((element) => { ... } )
 let scores = [68,75,70,80,85,90,82];
 const checkScore = scores.filter(score => score >= 80); //return คะแนนที่ >= 80 เท่านั้น
 console.log(checkScore);
+
+
+
 
 //ตัวอย่าง array.filter((element, index) => { ... } )
 let ids = [101,102,103,104,105];
 const checkId = ids.filter((id,index) => id < 102 || index == 3); //return idที่ <102 หรือ มี index อยู่ตำแหน่งที่ 3 เท่านั้น
 console.log(checkId);
 
+
+
+
 //ตัวอย่าง array.filter((element, index, array) => { ... } )
 let animals = ['ant','bird','cat','dog','frog'];
 const choose = animals.filter((animal,index,arr)=>{ //ไม่จำเป็นต้องใช้ parameter ครบทุกตัวก็ได้
-    arr.pop();                  //ดึง frog ออกจาก array นี้ (arr หมายถึง animals)
-    arr.push('rat');            //เพิ่ม rat ต่อท้าย array นี้
-    return animal.length==3;    //ดึง element ที่มีความยาวเท่ากับ 3 เท่านั้น     
+    arr.pop();                      //ดึง frog ออกจาก array นี้ (arr หมายถึง animals)
+    arr.push('rat');                //เพิ่ม rat ต่อท้าย array นี้
+    return animal.length==3;        //ดึง element ที่มีความยาวเท่ากับ 3 เท่านั้น     
 });
 console.log(choose);        
 
-let words = ['spray', 'limit', 'exuberant', 'destruction', 'elite', 'present'];
 
-const modifiedWords = words.filter((word, index, arr) => {
-  arr[index + 1] += ' extra';
-  console.log(arr);
-  return word.length < 6;
-});
-
-console.log(modifiedWords);
+//ตัวอย่าง array.filter((element, index, array) => { ... } )
+let nums = [0,1,2,3,4]
+const evenNum = nums.filter((num,index,array)=>{
+    array[index] += 1 ;     //แก้ไข element แต่ละตัว ให้ +1 เพิ่ม
+    console.log(array);     
+    return num%2===0;      
+})
+console.log(evenNum);
