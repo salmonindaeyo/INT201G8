20b9ff06d24a13f78c4b556f9d25b3d6e401a6c3
vichuta <vichuta.nice@mail.kmutt.ac.th>
2021-10-28 16:46:27 +0700

    add project

---
+++ Project/product.js
@@ -0,0 +1,14 @@
+function product (id,name,color,price,stock) {
+    this.id = id;
+    this.name = name;
+    this.color = color;
+    this.price = price;
+    this.stock = stock;
+}
+let products = []
+let product1 = new product("I6001","iphon6","green",3000,20);
+let product2 = new product("I6002","iphon6","white",3000,20);
+let product3 = new product("I6003","iphon6","red",3000,20);
+
+products.push(product1,product2,product3);
+export {products as product};
