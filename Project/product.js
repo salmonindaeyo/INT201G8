143dd590a16ba5d7a12cfd448ea440084e1df213
vichuta <vichuta.nice@mail.kmutt.ac.th>
2021-10-28 16:54:24 +0700

    finish code1

--- Project/product.js
+++ Project/product.js
@@ -6,9 +6,9 @@ function product (id,name,color,price,stock) {
     this.stock = stock;
 }
 let products = []
-let product1 = new product("I6001","iphon6","green",3000,20);
-let product2 = new product("I6002","iphon6","white",3000,20);
-let product3 = new product("I6003","iphon6","red",3000,20);
+let product1 = new product("I6001","iphone6","green",3000,20);
+let product2 = new product("I6002","iphone6","white",3000,20);
+let product3 = new product("I6003","iphone6","red",3000,20);
 
 products.push(product1,product2,product3);
 export {products as product};
