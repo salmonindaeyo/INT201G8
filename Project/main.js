e27bb923f644433ea72b94fed3bd1b15d4b729cb
vichuta <vichuta.nice@mail.kmutt.ac.th>
2021-10-28 16:52:35 +0700

    finish code1

--- Project/main.js
+++ Project/main.js
@@ -5,16 +5,22 @@ const divProductsEle = document.querySelector('#products');
 for (let i = 0; i < product.length; i++) {
 const divProduct = document.createElement('div');
 divProduct.setAttribute('id', product[i].id);
+
 const productId = document.createElement('p');
 productId.textContent = product[i].id;
+
 const productName = document.createElement('p');
 productName.textContent = product[i].name;
+
 const productPrice = document.createElement('p');
 productPrice.textContent = product[i].price + "Bath";
+
 const productColor = document.createElement('p');
 productColor.textContent = product[i].color;
+
 const productStock = document.createElement('p');
 productStock.textContent = product[i].stock;
+
 //<div><p></div>
 divProduct.appendChild(productId);
 divProduct.appendChild(productName);
