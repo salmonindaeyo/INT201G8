787bd23e208dc4accb1810326f15ec6fde5c6e7e
salmon <iamfhaa@gmail.com>
2021-11-03 00:01:42 +0700

    search

--- Project/main.js
+++ Project/main.js
@@ -1,6 +1,14 @@
 import {product} from './product.js'
-const divProductsEle = document.querySelector('#products');
 
+window.onload = startup;
+
+const divProductsEle = document.querySelector('#products');
+const answers = document.querySelectorAll("#fname")
+const divSearch = document.querySelector('#search');
+let divProductsEleShow;
+
+function startup () {
+    divProductsEleShow = document.createElement('div');
 
 for (let i = 0; i < product.length; i++) {
   
@@ -28,7 +36,7 @@ productStock.textContent = "Still have "+ product[i].stock + " units for sale.";
 const buttonProduct = document.createElement('button');
 buttonProduct.textContent = "Add to Cart" ;
 
-//<div><p></div>
+
 
 divProduct.appendChild(productImg);
 divProduct.appendChild(productName);
@@ -36,15 +44,120 @@ divProduct.appendChild(productPrice);
 divProduct.appendChild(productColor);
 divProduct.appendChild(productStock);
 divProduct.appendChild(buttonProduct);
-divProductsEle.appendChild(divProduct);
+
+divProductsEleShow.appendChild(divProduct);
+divProductsEle.appendChild(divProductsEleShow);
 }
 
-// <div class="card">
-        
-//<h1>Tailored Jeans</h1>
-//<p class="price">$19.99</p>
-//<p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p>
-//<p><button>Add to Cart</button></p>
-//</div>
+//const divSearch = document.querySelector('#search');
+
+//let inputName = document.createElement("input");
+//inputName.setAttribute("type", "text");
+//inputName.setAttribute("id", "phoneName");
+//inputName.setAttribute("onchange", "phoneSearch");
+
+//divSearch.appendChild(inputName);
+
+
+ //     let  addButton = document.createElement("input");
+ //   addButton.setAttribute("type", "button");
+ //   addButton.setAttribute("value", "clear");
+ //   addButton.setAttribute("onclick", "ClearPage ()");
+  //  document.body.appendChild(addButton);
+
+
+}
+
+
+//function ClearPage () {
+
+
+ //   const testt = document.createElement('h1');
+ //   testt.textContent = "555555555" ; 
+ //   divSearch.appendChild(testt);
+    
+//}
+    
+  
+answers.forEach((answers) => {
+    answers.addEventListener('change',phoneSearch2);
+})
+
+
+function phoneSearch2 () {
+
+            let CheckAnswer = false;
+            let CheckIndex = 0;
+        answers.forEach((answers)=>{
+            
+            for (let i = 0; i < product.length; i++) {
+                if (answers.value == product[i].name) {
+                    CheckAnswer = true; 
+                    CheckIndex = i;
+                }
+            }
+            
+        })
+
+        if (CheckAnswer) {
+            const divProductSearch = document.querySelector('#productSearch');
+           
+            let divProduct = document.createElement('div');
+            divProduct.setAttribute('id', "delete");
+    divProduct.setAttribute('class', "card");
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product[CheckIndex].id+".jpg");
+    productImg.setAttribute('width', "200");
+
+    const productName = document.createElement('h1');
+    productName.textContent = product[CheckIndex].name ;
+
+    const productPrice = document.createElement('p');
+    productPrice.setAttribute('class', "price");
+
+    productPrice.textContent = "$ " + product[CheckIndex].price ;
+
+    const productColor = document.createElement('p');
+    productColor.textContent = "The color of this phone is " + product[CheckIndex].color;
+
+    const productStock = document.createElement('p');
+    productStock.textContent = "Still have "+ product[CheckIndex].stock + " units for sale.";
+
+    const buttonProduct = document.createElement('button');
+    buttonProduct.textContent = "Add to Cart" ;
+
+
+
+    divProduct.appendChild(productImg);
+    divProduct.appendChild(productName);
+    divProduct.appendChild(productPrice);
+    divProduct.appendChild(productColor);
+    divProduct.appendChild(productStock);
+    divProduct.appendChild(buttonProduct);
+
+
+    divProductSearch.appendChild(divProduct);
+
+    divProductsEleShow.remove();
+
+        }
+
+
+        if (CheckAnswer == false) {
+            const test = document.querySelector('#delete');
+            test.remove();
+            startup();
+            
+        }
+
+
+
+}
+
+
+
+
+
 
 
--- Project/product.js
+++ Project/product.js
@@ -6,6 +6,7 @@ function product (id,name,color,price,stock) {
     this.stock = stock;
 }
 let products = []
+
 let product1 = new product("I001","Iphone 13 Pro","Blue",1290,10);
 let product2 = new product("I002","Iphone 13 mini","Red",1200,7);
 let product3 = new product("I003","Iphone 12","Purple",730,11);
